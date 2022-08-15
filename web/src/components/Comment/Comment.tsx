4ecc5151385a6a97c22fcb2d897b16f3cb48f4c8
OhHiMerq <dimailigv@gmail.com>
2022-08-15 21:28:20 +0800

    generate Comment component

---
+++ web/src/components/Comment/Comment.tsx
@@ -0,0 +1,29 @@
+const formattedDate = (datetime: ConstructorParameters<typeof Date>[0]) => {
+  const parsedDate = new Date(datetime)
+  const month = parsedDate.toLocaleString('default', { month: 'long' })
+  return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
+}
+
+interface Props {
+  comment: {
+    name: string
+    createdAt: string
+    body: string
+  }
+}
+
+const Comment = ({ comment }: Props) => {
+  return (
+    <div>
+      <header>
+        <h2>{comment.name}</h2>
+        <time dateTime={comment.createdAt}>
+          {formattedDate(comment.createdAt)}
+        </time>
+      </header>
+      <p>{comment.body}</p>
+    </div>
+  )
+}
+
+export default Comment
