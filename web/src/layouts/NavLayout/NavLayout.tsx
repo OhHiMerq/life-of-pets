6a08d1a8b29b3d46d811e0f4ca9ab88bed06f7aa
OhHiMerq <dimailigv@gmail.com>
2022-08-16 16:06:51 +0800

    add styles nav

--- web/src/layouts/NavLayout/NavLayout.tsx
+++ web/src/layouts/NavLayout/NavLayout.tsx
@@ -1,45 +1,80 @@
 import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
-
+import 'src/index.css'
 type NavLayoutProps = {
   children?: React.ReactNode
 }
-
+const LinkStyle = {
+  textDecoration: 'none',
+  fontSize: '1.5rem',
+  color: 'white',
+}
 const NavLayout = ({ children }: NavLayoutProps) => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
   return (
     <>
-      <header>
-        <div>
-          <h1>MICROBLOG</h1>
-          {isAuthenticated ? (
+      <header
+        style={{
+          width: '90%',
+          height: '5em',
+          display: 'flex',
+          padding: '0 5%',
+          backgroundColor: '#348bd1',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <div style={{ fontSize: '2rem', color: 'white', fontWeight: 'bold' }}>
+          MICROBLOG
+        </div>
+        {isAuthenticated ? (
+          <>
+            <nav className="nav">
+              <ul
+                style={{
+                  listStyle: 'none',
+                  display: 'flex',
+                  alignItems: 'baseline',
+                }}
+              >
+                <li style={{ marginLeft: '3rem' }}>
+                  <Link to={routes.home()} style={LinkStyle}>
+                    Home
+                  </Link>
+                </li>
+                <li style={{ marginLeft: '3rem' }}>
+                  <Link to={routes.follow()} style={LinkStyle}>
+                    Follow
+                  </Link>
+                </li>
+                <li style={{ marginLeft: '3rem' }}>
+                  <Link to={routes.profile()} style={LinkStyle}>
+                    Profile
+                  </Link>
+                </li>
+              </ul>
+            </nav>
             <div>
-              <span>Logged in as {currentUser.email}</span>{' '}
-              <nav>
-                <ul>
-                  <li>
-                    <Link to={routes.home()}>Home</Link>
-                  </li>
-                  <li>
-                    <Link to={routes.follow()}>Follow</Link>
-                  </li>
-                  <li>
-                    <Link to={routes.profile()}>Profile</Link>
-                  </li>
-                </ul>
-              </nav>
-              <button type="button" onClick={logOut}>
+              <span
+                style={{
+                  fontSize: '1rem',
+                  color: 'white',
+                }}
+              >
+                Logged in as {currentUser.email}
+              </span>{' '}
+              <button type="button" onClick={logOut} className="btn-logout">
                 Logout
               </button>
             </div>
-          ) : (
-            <div>
-              <Link to={routes.login()}>Login</Link>
-            </div>
-          )}
-        </div>
+          </>
+        ) : (
+          <div>
+            <Link to={routes.login()}>Login</Link>
+          </div>
+        )}
       </header>
-      <main>{children}</main>
+      <main style={{ margin: 'auto', maxWidth: '35rem' }}>{children}</main>
     </>
   )
 }
