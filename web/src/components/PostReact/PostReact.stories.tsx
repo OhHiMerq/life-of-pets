391af278e6d11e2985458437241b0ab6a4e0333d
OhHiMerq <dimailigv@gmail.com>
2022-08-21 12:43:50 +0800

    generate component PostReact

---
+++ web/src/components/PostReact/PostReact.stories.tsx
@@ -0,0 +1,7 @@
+import PostReact from './PostReact'
+
+export const generated = (args) => {
+  return <PostReact {...args} />
+}
+
+export default { title: 'Components/PostReact' }
---
+++ web/src/components/PostReact/PostReact.tsx
@@ -0,0 +1,10 @@
+const PostReact = () => {
+  return (
+    <div>
+      <h2>{'PostReact'}</h2>
+      <p>{'Find me in ./web/src/components/PostReact/PostReact.tsx'}</p>
+    </div>
+  )
+}
+
+export default PostReact
