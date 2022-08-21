a411b6276e842f879f878a3ced89886ee78cace3
OhHiMerq <dimailigv@gmail.com>
2022-08-21 14:37:23 +0800

    reload posts after post deletion

--- web/src/components/DeletePost/DeletePost.tsx
+++ web/src/components/DeletePost/DeletePost.tsx
@@ -1,4 +1,7 @@
 import { useMutation } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/dist/toast'
+import { QUERY as FollowedPostsQuery } from 'src/components/FollowedArticlesCell'
+import { QUERY as UserPostsQuery } from 'src/components/ArticlesCell'
 
 const DELETE_POST = gql`
   mutation DeletePostMutation($id: Int!) {
@@ -10,9 +13,19 @@ const DELETE_POST = gql`
 `
 
 const DeletePost = ({ article, currentUser }) => {
-  const [deletePost, { loading, error }] = useMutation(DELETE_POST)
+  const [deletePost, { loading, error }] = useMutation(DELETE_POST, {
+    onCompleted: () => {
+      toast.success('Post Deleted!')
+    },
+    refetchQueries: [
+      { query: FollowedPostsQuery, variables: { userId: currentUser.id } },
+      { query: UserPostsQuery, variables: { userId: currentUser.id } },
+    ],
+  })
+
   return (
     <>
+      <Toaster />
       {article.userId == currentUser.id ? (
         <span>
           <button
